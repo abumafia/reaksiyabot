9425ecbc70de3b96ee6867134dccd336f6319c98
HALLAYM LLC <158750555+abumafia@users.noreply.github.com>
2026-01-06 15:29:32 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // ==================== KONFIGURATSIYA ====================
 const BOT_TOKEN = process.env.BOT_TOKEN || '7412314295:AAHYB804OToAPUQiC-b6Ma6doBtMCHETmQU';
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://abumafia0:abumafia0@abumafia.h1trttg.mongodb.net/st2bot?appName=abumafia';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://abumafia0:abumafia0@abumafia.h1trttg.mongodb.net/st3bot?appName=abumafia';
 const ADMIN_IDS = (process.env.ADMIN_IDS || '6606638731').split(',').filter(id => id.trim() !== '').map(id => parseInt(id));
 const WEBHOOK_DOMAIN = process.env.WEBHOOK_DOMAIN || process.env.RENDER_EXTERNAL_URL;
 const WEBHOOK_PATH = process.env.WEBHOOK_PATH || '/webhook';
